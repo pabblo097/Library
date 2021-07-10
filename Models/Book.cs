18136c742bd821c15f7d5fde101dcfd6293dc93d
Test Account <test@gmail.com>
2021-07-10 19:16:40 +0200

    new prop in Book.cs test

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,5 +10,6 @@ namespace Library.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
+        public string TestProp { get; set; }
     }
 }
