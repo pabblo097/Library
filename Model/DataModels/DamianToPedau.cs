ab4b59f3fc47c104cbbfcb802e0a6ad1f9274c23
Pawel Wiewiora <kaczka097@gmail.com>
2021-07-10 20:47:24 +0200

    pedau

---
+++ Model/DataModels/DamianToPedau.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Model.DataModels
+{
+    public class DamianToPedau
+    {
+        public string Gowno { get; set; }
+    }
+}
