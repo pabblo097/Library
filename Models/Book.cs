231a0fc0f8a34a45ccc124b5b3347b37c251f1a3
Pawel Wiewiora <kaczka097@gmail.com>
2021-07-10 19:48:18 +0200

    test2

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,6 +10,5 @@ namespace Library.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
-        public string TestProp { get; set; }
     }
 }
