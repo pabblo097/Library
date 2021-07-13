c8d3bcad43ff164191dbca658b5d815464ace90f
Pawel Wiewiora <kaczka097@gmail.com>
2021-07-13 22:20:49 +0200

    dodano wiecej ksiazek

--- BooksData.sql
+++ BooksData.sql
@@ -9,3 +9,9 @@ values ('Samotna','Lisa Gardner', 2020, 'Albatros', 'Bobby Dodge, snajper policj
 
 insert into Books (Name, Author, Year, Publisher, Description, Genre, NumberOfPages, Count) 
 values ('Zielona Mila','Stephen King', 2020, 'Albatros', 'Akcja powieści, zbliżonej klimatem do głośnej noweli tego samego autora, Skazani na Shawshank, toczy się w Ameryce lat 30. Jej bohaterowie to więźniowie oczekujący na wykonanie kary śmierci i pilnujący ich strażnicy.', 'fantastyka', 416, 3);
+
+insert into Books (Name, Author, Year, Publisher, Description, Genre, NumberOfPages, Count) 
+values ('Poszukiwania','Nora Roberts', 2020, 'Świat Książki', 'Fiona, niedoszła ofiara seryjnego mordercy, usiłuje powrócić do normalnego życia. Na pięknej wyspie nieopodal Seattle prowadzi szkołę dla psów i podejmuje akcje ratunkowe. Niezbędne jej spokój i samotność zakłóca Simon, szorstki i skryty twórca artystycznych mebli, mający kłopot ze swym psem.', 'literatura obyczajowa', 448, 3);
+
+insert into Books (Name, Author, Year, Publisher, Description, Genre, NumberOfPages, Count) 
+values ('Wybaczam ci','Remigiusz Mróz', 2020, 'Czwarta Strona', 'Ina Kobryn prowadzi spokojne, uporządkowane życie. Spełnia się w pracy, wraz z mężem stara się o dziecko i właściwie nie ma powodów do zmartwień. Wszystko zmienia się, gdy pewnej nocy siada przed laptopem i dostrzega, że mąż nie wylogował się ze swojego konta na Facebooku. Jej wzrok pada na krótką wiadomość o treści „wybaczam ci”, pochodzącą od nieznajomej dziewczyny.', 'kryminał', 432, 3);
