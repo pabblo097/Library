45ba43381694166d168aa326718af3155c72d465
Damian Rekorajski <francisco2k10@gmail.com>
2021-07-16 14:17:46 +0200

    title>name

--- Models/DataModels/Book.cs
+++ Models/DataModels/Book.cs
@@ -10,7 +10,7 @@ namespace Library.Models.DataModels
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string Author { get; set; }
         public int Year { get; set; }
         public string Publisher { get; set; }
